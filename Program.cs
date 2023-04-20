2b22b0257251b602f91723e2cb2b3ec756c6f502
Renatoomagat <renatoomagat@mail.ru>
2023-04-20 19:16:14 +0300

    Обновление

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите имя: ");
+var name = Console.ReadLine();
+Console.Write("Возраст: ");
+var age = Console.ReadLine();
+Console.WriteLine("Ваше имя {0}, возраст {1}", name, age);
+Console.Write("Введите дату рожденя: ");
+var birthdate = Console.ReadLine();
+Console.WriteLine("Ваша дата рождения {0}", birthdate);
